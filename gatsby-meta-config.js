7c84d0af899b0db7aa79a1e6e00475a7373c66e1
saengmotmi <ohjtack@gmail.com>
2020-05-10 23:58:23 +0900

    post

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -14,7 +14,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'saengmotmi/saengmotmi.github.io', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
